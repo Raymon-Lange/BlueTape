cf0467bb456bb952b1d9e5c02a52aaa98b927a39
Raymon.Lange <raymon.lange@gmail.com>
2023-06-02 15:03:50 -0500

    Added Sprite class

---
+++ sprite.py
@@ -0,0 +1,39 @@
+import os,sys, random, math
+import pygame
+from os import listdir
+from os.path import isfile, join
+
+class Sprite():
+    def __init__(self):
+        self.allSprites = {}
+
+    def flip(self, sprites):
+        return [pygame.transform.flip(sprite, True, False) for sprite in sprites]
+    
+    def loadSpriteSheet(self, dir1, dir2, width, height, direction=False):
+        path = join("assets", dir1, dir2)
+        images = [f for f in listdir(path) if isfile(join(path, f))]
+
+        for image in images: 
+            spriteSheet = pygame.image.load(join(path,image)).convert_alpha()
+
+            sprites= []
+            for i in range(spriteSheet.get_width() // width):
+                surface = pygame.Surface((width, height), pygame.SRCALPHA, 32)
+                rect = pygame.Rect(i * width, 0, width, height)
+                surface.blit(spriteSheet,(0,0), rect)
+                sprites.append(pygame.transform.scale2x(surface))
+
+            print(image, len(sprites))
+        
+            if direction:
+                self.allSprites[image.replace(".png", "") + "_right"] = sprites
+                self.allSprites[image.replace(".png", "") + "_left"] = self.flip(sprites)
+            else:
+                self.allSprites[image.replace(".png", "")] = sprites
+
+    def getSprite(self, name, pos):
+        return self.allSprites[name][pos]
+    
+    def getAllSprint(self):
+        return self.allSprites
