d142257a72d6b5e9fd6babb7b0dc0efa52d396c2
Raymon Lange <raymon.lange@gmail.com>
2023-06-03 21:21:11 -0500

    Added more than block type

--- block.py
+++ block.py
@@ -2,19 +2,25 @@ import pygame
 from object import Object
 from os.path import join
 
+BLOCKS = {"Grass": pygame.Rect(96, 0, 96, 96),
+           "BrownBar": pygame.Rect(190, 0, 100, 32)}
+
 class Block(Object):
-    def __init__(self, x, y, size):
-        super().__init__(x, y, size, size, None)
-        block = self.get_block(size)
+    def __init__(self, xPos, yPos, width, height , name):
+        super().__init__(xPos, yPos, width, height, None)
+        block = self.getBlock(width, height,name)
         self.image.blit(block, (0, 0))
         self.mask = pygame.mask.from_surface(self.image)
 
-    def get_block(self, size):
+    def getBlock(self, width, height, name):
         path = join("assets", "Terrain", "Terrain.png")
         image = pygame.image.load(path).convert_alpha()
-        surface = pygame.Surface((size, size), pygame.SRCALPHA, 32)
-        rect = pygame.Rect(96, 0, size, size)
+        surface = pygame.Surface((width, height), pygame.SRCALPHA, 32)
+        rect = BLOCKS[name]
         surface.blit(image, (0, 0), rect)
         return pygame.transform.scale2x(surface)
+
     
 
+#190 240
+#16
--- level.py
+++ level.py
@@ -9,11 +9,14 @@ class Level():
 
     def loadLevel(self):
         block_size = 96
-        floor = [Block(i * block_size, self.height - block_size, block_size)
+        floor = [Block(i * block_size, self.height - block_size, block_size,block_size, "Grass")
              for i in range(-self.width // block_size, (self.width * 2) // block_size)]
         
-        floor.append(Block(0, self.height - block_size * 2, block_size))
-        floor.append(Block(block_size * 3, self.height - block_size * 4, block_size))
+        floor.append(Block(0, self.height - block_size * 2, block_size,block_size, "Grass"))
+        floor.append(Block(block_size * 3, self.height - block_size * 4, block_size,block_size, "Grass"))
+        floor.append(Block((block_size * 4)-5, self.height - block_size * 4, 100,32, "BrownBar"))
+        floor.append(Block((block_size * 7), self.height - block_size * 4, 100,32, "BrownBar"))
+
         
         return floor
 
